46269ab9b79b5dcbc88fa7bd9235d6b3958d8282
Jgrady4 <jgrady4@elon.edu>
2015-12-01 15:35:20 -0500

    first commit

---
+++ quotes.js
@@ -0,0 +1,8 @@
+var randomQuotes = [
+
+	{quote: "I am a frog.", author: "Kermit"},
+	{quote: "I am a pig.", author: "Miss Piggy"},
+	{quote: "I am a dog.", author: "Rowlf"},
+	{quote: "I am a weirdo.", author: "Gonzo"}
+
+];
